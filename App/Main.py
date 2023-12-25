8706013556e02fe1dc512f1e3bc281ec834b70a6
Simon Noble <expectedb@outlook.com>
2023-12-25 15:57:19 -0500

    Refactored to make the datastructure more clear

--- App/Main.py
+++ App/Main.py
@@ -1,14 +1,6 @@
-import random
-
-from App.Game.Dilemma import Dilemma
 from App.Game.RewardSimulator import RewardSimulator
-from App.Game.StrategyFactory import StrategyFactory
-from App.Strategies.AllwaysBetray import AllwaysBetray
-from App.Strategies.AllwaysCooperate import AllwaysCooperate
+from App.Strategies.StrategyFactory import StrategyFactory
 from App.RewardMatricies.BasicReward import BasicReward
-from App.Game.Duel import Duel
-from App.Strategies.TitForTat import TitForTat
-from App.Strategies.TitForTwoTats import TitForTwoTats
 
 
 def main():
---
+++ App/Strategies/StrategyFactory.py
@@ -0,0 +1,41 @@
+from App.Strategies.StrategyImplementations.AllwaysBetray import AllwaysBetray
+from App.Strategies.StrategyImplementations.AllwaysCooperate import AllwaysCooperate
+from App.Strategies.StrategyImplementations.DistrustingTitForTat import DistrustingTitForTat
+from App.Strategies.StrategyImplementations.Exploiter import Exploiter
+from App.Strategies.StrategyImplementations.Random import Random
+from App.Strategies.StrategyImplementations.ReverseTitForTat import ReverseTitForTat
+from App.Strategies.StrategyInterface import StrategyInterface
+from App.Strategies.StrategyImplementations.TitForTat import TitForTat
+from App.Strategies.StrategyImplementations.TitForTwoTats import TitForTwoTats
+from App.Strategies.StrategyImplementations.TwoThirdsBetray import TwoThirdsBetray
+from App.Strategies.StrategyImplementations.TwoThirdsCooperate import TwoThirdsCooperate
+from App.Strategies.StrategyImplementations.Vindictive import Vindictive
+
+
+class StrategyFactory:
+
+    @staticmethod
+    def get_strategies() -> list[StrategyInterface]:
+        """
+
+        return a list of all the strategies
+
+        More conditions may be added later
+        :return:
+        """
+
+        strategies = []
+
+        strategies.append(Random())
+        strategies.append(TitForTat())
+        strategies.append(TitForTwoTats())
+        strategies.append(AllwaysBetray())
+        strategies.append(AllwaysCooperate())
+        strategies.append(DistrustingTitForTat())
+        strategies.append(ReverseTitForTat())
+        strategies.append(TwoThirdsBetray())
+        strategies.append(TwoThirdsCooperate())
+        strategies.append(Vindictive())
+        strategies.append(Exploiter())
+
+        return strategies
